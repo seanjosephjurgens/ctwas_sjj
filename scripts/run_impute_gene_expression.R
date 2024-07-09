d3e6fb782a55d167d13af019d46113c06527ca48
seanjosephjurgens <66628479+seanjosephjurgens@users.noreply.github.com>
2024-07-09 13:33:03 +0200

    Update and rename run_impute_gene_expression.R to scripts/run_impute_gene_expression.R

--- scripts/run_impute_gene_expression.R
+++ scripts/run_impute_gene_expression.R
@@ -2,11 +2,14 @@
 
 args = commandArgs(trailingOnly=TRUE)
 
+base_wd <- "/home/jupyter/cTWAS/"
+
 formatted_sumstats <- as.character(args[1])
 gwas_name <- as.character(args[2])
 tissue <- as.character(args[3])
 total_chunks <- as.numeric(args[4])
 chunk <- as.numeric(args[5])
+try(base_wd <- as.character(args[6]))
 
 library(RSQLite)
 library(ctwas)
@@ -16,7 +19,7 @@ z_snp <- readRDS(formatted_sumstats)
 
 #### formatting prediction models #### 
 #specify the weight to remove lncRNA from
-weight <- paste0("~/cTWAS/data/GTEx_models/weights/eqtl/mashr/mashr_", tissue, ".db")
+weight <- paste0(base_wd, "data/GTEx_models/weights/eqtl/mashr/mashr_", tissue, ".db")
 #read the PredictDB weights
 sqlite <- dbDriver("SQLite")
 db = dbConnect(sqlite, weight)
@@ -37,23 +40,23 @@ weights_table <- weights_table[weights_table$gene %in% extra_table$gene,]
 weight_info = read.table(gzfile(paste0(tools::file_path_sans_ext(weight), ".txt.gz")), header = T)
 weight_info <- weight_info[weight_info$GENE %in% extra_table$gene,]
 #write the .db file and the covariances
-if (!file.exists(paste0("~/cTWAS/data/GTEx_models/weights_nolnc/mashr_", tissue, "_nolnc_subset_chunk", chunk, ".db"))){
-  db <- dbConnect(sqlite, paste0("~/cTWAS/data/GTEx_models/weights_nolnc/mashr_", tissue, "_nolnc_subset_chunk", chunk, ".db"))
+if (!file.exists(paste0(base_wd, "data/GTEx_models/weights_nolnc/mashr_", tissue, "_nolnc_subset_chunk", chunk, ".db"))){
+  db <- dbConnect(sqlite, paste0(base_wd, "data/GTEx_models/weights_nolnc/mashr_", tissue, "_nolnc_subset_chunk", chunk, ".db"))
   dbWriteTable(db, "extra", extra_table)
   dbWriteTable(db, "weights", weights_table)
   dbDisconnect(db)
   
-  weight_info_gz <- gzfile(paste0("~/cTWAS/data/GTEx_models/weights_nolnc/mashr_", tissue, "_nolnc_subset_chunk", chunk, ".txt.gz"), "w")
+  weight_info_gz <- gzfile(paste0(base_wd, "data/GTEx_models/weights_nolnc/mashr_", tissue, "_nolnc_subset_chunk", chunk, ".txt.gz"), "w")
   write.table(weight_info, weight_info_gz, sep=" ", quote=F, row.names=F, col.names=T)
   close(weight_info_gz)
 }
 #specify the weight for the analysis
-weight_subset <- paste0("~/cTWAS/data/GTEx_models/weights_nolnc/mashr_", tissue, "_nolnc_subset_chunk", chunk, ".db")
+weight_subset <- paste0(base_wd, "data/GTEx_models/weights_nolnc/mashr_", tissue, "_nolnc_subset_chunk", chunk, ".db")
 
 #ld_pgenfs <- paste0("~/cTWAS/data/GTEx_LDref/", paste0("GTEx_EUR_chr", 1:22, ".bed"))
-ld_R_dir <- "/home/jupyter/cTWAS/data/GTEx_LDref/matrices_0.1/LDR_b38_cova/"
+ld_R_dir <- paste0(base_wd, "data/GTEx_LDref/matrices_0.1/LDR_b38_cova/")
 
-outputdir <- paste0("~/cTWAS/results/imputed_gene_expression/", gwas_name, "/", tissue, "/chunk", chunk)
+outputdir <- paste0(base_wd, "results/imputed_gene_expression/", gwas_name, "/", tissue, "/chunk", chunk)
 outname <- paste0(gwas_name, "_chunk", chunk)
 dir.create(outputdir, showWarnings=F, recursive=T)
 
